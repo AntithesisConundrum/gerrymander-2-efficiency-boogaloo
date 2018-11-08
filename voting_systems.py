8af7cdfd70863a1660cdea46bdae544206e975bd
AntithesisConundrum <clp9@rice.edu>
2018-11-08 16:42:38 -0600

    Added docstring

---
+++ voting_systems.py
@@ -0,0 +1,4 @@
+"""
+This module is a number of different methods representing different voting systems.
+Each function should take a list of vote-orderings and return a (winner, heuristic) pair
+"""
