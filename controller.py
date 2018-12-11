ec4cdbe6ca92fcebf08da523a90997bf3a9fcfd4
AntithesisConundrum <clp9@rice.edu>
2018-12-11 02:53:08 -0600

    Added run_full_experiment functionality

--- controller.py
+++ controller.py
@@ -8,7 +8,8 @@ from voting_systems.classic_stv import classic_single_transferrable_vote
 from voting_systems.complete_stv import complete_single_transferrable_vote
 from voting_systems.honest_fptp import honest_fptp
 from voting_systems.strategic_fptp import strategic_fptp
-
+from random import shuffle
+from collections import defaultdict
 
 def calculate_top_two_ratio(ordering, wasted_votes):
     """
@@ -29,6 +30,15 @@ def calculate_total_ratio(ordering, wasted_votes):
     for loser in ordering[1:]:
         loser_total += wasted_votes[loser]
     return float(wasted_votes[winner]) / loser_total
+
+def calculate_efficiency_gap(wasted_votes, num_voters):
+    """
+    Calculate EG given wasted votes.
+    Assumes DEMOCRAT won.
+    """
+    winner = wasted_votes[DEMOCRAT]
+    loser = wasted_votes[REPUBLICAN] + wasted_votes[LIBERTARIAN] + wasted_votes[GREEN]
+    return (winner-loser)/float(num_voters)
 
 def run_experiment(out_name, distribution):
     out_file = open(out_name, 'wb')
@@ -114,8 +124,64 @@ def run_experiment(out_name, distribution):
             complete_stv_top_two_ratio,
             complete_stv_total_ratio])
 
-if __name__ == "__main__":
-    run_experiment("basic_results.csv", BASIC_ORDER_PROBS)
-    run_experiment("maine_results.csv", MAINE_PROBS)
+def run_full_experiment(out_name, distribution):
+    out_file = open(out_name, 'wb')
+    out_writer = csv.writer(out_file)
 
+    out_writer.writerow([
+        "Honest_FPTP_Efficiency_Gap",
+        "Strategic_FPTP_Efficiency_Gap",
+        "Approval_Efficiency_Gap",
+        "Borda_Efficiency_Gap",
+        "Classic_STV_Efficiency_Gap",
+        "Complete_STV_Efficiency_Gap",
+        ])
+
+    num_trials = 1000
+    num_districts = 10
+    voters_per_district = 1000
+    num_voters = num_districts * voters_per_district
+
+    # Get num_trials electorates
+    for _ in xrange(num_trials):
+        electorate = generate_voters(num_voters, distribution)
+
+        # Map each voting system to a mapping from party to WV
+        sys_party_wv = defaultdict(lambda: defaultdict(int))
+
+        # Keep track of number of districts won by winning_party
+        districts_won = 0
+
+        # For each district, calculate wasted vote
+        for district_no in xrange(num_districts):
+            # Find district electorate
+            district_electorate = electorate[district_no*voters_per_district:(district_no+1)*voters_per_district]
+
+            systems = [
+                (honest_fptp, "Honest_FPTP"),
+                (strategic_fptp, "Strategic_FPTP"),
+                (approval_voting, "Approval"),
+                (borda_count_vote, "Borda"),
+                (classic_single_transferrable_vote, "Classic_STV"),
+                (complete_single_transferrable_vote, "Complete_STV")
+            ]
+
+            for method, name in systems:
+                ordering, wasted_votes = method(district_electorate)
+                for party in ordering:
+                    sys_party_wv[name][party] += wasted_votes[party]
+
+        # For each district plan, calculate efficiency gap
+        out_writer.writerow([calculate_efficiency_gap(sys_party_wv["Honest_FPTP"], num_voters),
+            calculate_efficiency_gap(sys_party_wv["Strategic_FPTP"], num_voters),
+            calculate_efficiency_gap(sys_party_wv["Approval"], num_voters*2),
+            calculate_efficiency_gap(sys_party_wv["Borda"], num_voters*(3+2+1)),
+            calculate_efficiency_gap(sys_party_wv["Classic_STV"], num_voters),
+            calculate_efficiency_gap(sys_party_wv["Complete_STV"], num_voters),
+        ])
+
+if __name__ == "__main__":
+    #run_experiment("basic_results.csv", BASIC_ORDER_PROBS)
+    #run_experiment("maine_results.csv", MAINE_PROBS)
+    run_full_experiment("maine_full_results.csv", MAINE_PROBS)
 
